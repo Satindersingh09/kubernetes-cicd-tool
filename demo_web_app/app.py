22e5bf0f94f15cc1bdd1bcde15284185dd9cdfc0
satinder singh <satindersingh0902@gmail.com>
2026-08-31 07:10:18 +0200

    Update application version

--- demo_web_app/app.py
+++ demo_web_app/app.py
@@ -10,7 +10,7 @@ def home():
 
 @app.route("/health")
 def health():
-    return {"status": "healthy", "service": "cicd-demo-app"}, 200
+    return {"status": "healthy", "service": "cicd-demo-app" "version": "v2"}, 200
 
 
 if __name__ == "__main__":
